64f52153868649a3231dcb737222260db1220b2d
Qobi1 <tolaganov03@list.ru>
2023-05-01 12:43:37 +0500

    commit

--- webappbot/settings.py
+++ webappbot/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'webappbot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'railway',
-#         'USER': 'postgres1',
-#         'PASSWORD': 'pbDJOFFF4aPuP55oiDBW',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'pbDJOFFF4aPuP55oiDBW',
+        'HOST': 'containers-us-west-102.railway.app',
+        'PORT': '5701',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
